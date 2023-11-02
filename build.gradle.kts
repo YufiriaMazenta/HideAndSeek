f14e526e8a7c65d253b9674b5c0c90e7a5aef71d
YufiriaMazenta <72661215+YufiriaMazenta@users.noreply.github.com>
2023-11-02 09:36:47 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import java.text.SimpleDateFormat
+
+plugins {
+    id("java-library")
+    id("maven-publish")
+    id("com.github.johnrengelman.shadow").version("7.1.2")
+}
+
+repositories {
+    mavenLocal()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://repo.maven.apache.org/maven2/")
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:24.0.1")
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    implementation("com.github.YufiriaMazenta:CrypticLib:1.0.9")
+}
+
+group = "com.example"
+version = "1.0.2"
+var mainClass = "${rootProject.group}.${rootProject.name.lowercase()}.Example"
+var pluginVersion: String = version.toString() + "-" + SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
+    }
+}
+
+tasks {
+    val props = HashMap<String, String>()
+    props["version"] = pluginVersion
+    props["main"] = mainClass
+    props["name"] = rootProject.name
+    processResources {
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    shadowJar {
+        relocate("crypticlib", "${rootProject.group}.${rootProject.name.lowercase()}.crypticlib")
+        archiveFileName.set("${rootProject.name}-${version}.jar")
+    }
+    assemble {
+        dependsOn(shadowJar)
+    }
+}
---
+++ src/main/java/com/example/crypticlibexample/Example.java
@@ -0,0 +1,18 @@
+package com.example.crypticlibexample;
+
+import crypticlib.BukkitPlugin;
+import crypticlib.CrypticLib;
+
+public class Example extends BukkitPlugin {
+
+    @Override
+    public void enable() {
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void disable() {
+        //TODO
+    }
+
+}
