b4ff967e1146cfcde608fa0f66e43375f7faf2b9
ChiyodaXiaoYi <linjiayi357159@gmail.com>
2023-11-07 21:41:51 +0800

    [1.0.0-dev17]由于CrypticLib仓库重新创建，更新版本为0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,11 @@ dependencies {
     compileOnly("org.jetbrains:annotations:24.0.1")
     compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
     compileOnly("LibsDisguises:LibsDisguises:10.0.38")
-    implementation("com.github.YufiriaMazenta:CrypticLib:1.1.0")
+    implementation("com.github.YufiriaMazenta:crypticlib:0.0.1")
 }
 
 group = "com.github.yufiriamazenta"
-version = "1.0.0-dev16"
+version = "1.0.0-dev17"
 var mainClass = "${rootProject.group}.${rootProject.name.lowercase()}.HideAndSeek"
 var pluginVersion: String = version.toString() + "-" + SimpleDateFormat("yyyyMMdd").format(System.currentTimeMillis())
 java.sourceCompatibility = JavaVersion.VERSION_17
